1fd9556430178f538c501837c97870d23a2d6d96
Samuels Desktop <cool23man23@gmail.com>
2025-02-18 12:54:55 +0100

    test-produkter

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -14,21 +14,53 @@ export async function GET() {
             description: "Shirt description",
             tag: "tag1",
             price: Math.floor(Math.random() * 900) + 100,
+            available: true,
+            images: {
+                createMany: {
+                    data: [{
+                        url: "/images/red.jpg"
+                    }]
+                }
+            }
         },{
             name: "T-Shirt 2",
             description: "Shirt description",
             tag: "tag1",
             price: Math.floor(Math.random() * 900) + 100,
+            available: true,
+            images: {
+                createMany: {
+                    data: [{
+                        url: "/images/black.jpg"
+                    }]
+                }
+            }
         },{
             name: "T-Shirt 3",
             description: "Shirt description",
             tag: "tag2",
             price: Math.floor(Math.random() * 900) + 100,
+            available: true,
+            images: {
+                createMany: {
+                    data: [{
+                        url: "/images/yellow.jpg"
+                    }]
+                }
+            }
         },{
             name: "T-Shirt 4",
             description: "Shirt description",
             tag: "tag3",
             price: Math.floor(Math.random() * 900) + 100,
+            available: true,
+            images: {
+                createMany: {
+                    data: [{
+                        url: "/images/blue.jpg"
+                    }]
+                }
+            }
         },
     ]
 
